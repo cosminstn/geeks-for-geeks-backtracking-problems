84b0ecf1071853376bdc1a89bc78017c91d3130b
Cosmin Stoian <cosmyn9708@gmail.com>
2021-01-18 22:35:38 +0200

    Added a variable that should hold all solutions and now returning that in solve(). Still only displays the first solution.

--- word-break-problem.py
+++ word-break-problem.py
@@ -20,29 +20,29 @@
 dict = ["i", "like", "sam", "sung", "samsung", "mobile", "ice", "and", "cream", "icecream", "man", "go", "mango"]
 
 
-def solve_part(to_solve, solution):
+def solve_part(to_solve, solution, all_solutions):
     if len(to_solve) == 0:
+        all_solutions.append(solution)
         return True
     if to_solve in dict:
         solution.append(to_solve)
+        all_solutions.append(solution)
         return True
-    print(f"DEBUG: Called solvePart for input: {to_solve} solution: ")
-    print(f"DEBUG {solution}")
+    print(f"DEBUG: Called for to_solve: {to_solve} & current solution: {solution}")
     for word in dict:
         if to_solve.startswith(word):
             solution.append(word)
-            return solve_part(to_solve[len(word):], solution)
+            return solve_part(to_solve[len(word):], solution, all_solutions)
 
     return False
 
 
 def solve(to_solve):
-    solution = []
-    solve_part(to_solve, solution)
+    all_solutions = []
+    solve_part(to_solve, [], all_solutions)
     print('Final solution: ')
-    return solution
+    return all_solutions
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(solve("ilikesamsungmobile"))
